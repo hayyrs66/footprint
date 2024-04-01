0d92d1ffc85ca602f65e39b60b489715016a6a6b
hayyrs66 <rayrtsx@proton.me>
2024-04-01 07:54:35 -0600

    Update credentials path in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+User
 import fs from 'fs/promises';
 import path from 'path';
 import process from 'process';
@@ -10,7 +11,7 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'src/utils/credentials.json');
 
 /**
  * Reads previously authorized credentials from the save file.
