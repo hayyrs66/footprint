f2a07a554edd9bff72c2fc0d63c8be1b5ca6bc39
hayyrs66 <rayrtsx@proton.me>
2024-04-27 22:56:39 -0600

    Add tailwind integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
-
 import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()],
+  integrations: [auth(), tailwind()],
   output: "server",
   adapter: node({
-    mode: "middleware",
-    
+    mode: "middleware"
   }),
   vite: {
     build: {
