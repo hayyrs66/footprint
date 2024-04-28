f63183f6fc8b9b1011acad673a811171726e225e
hayyrs66 <rayrtsx@proton.me>
2024-04-28 11:39:44 -0600

    Add astro-icon integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), tailwind()],
+  integrations: [auth(), tailwind(), icon()],
   output: "server",
   adapter: node({
     mode: "middleware"
