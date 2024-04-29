ac6263da061c129636ab235aa63ad9858014b573
hayyrs66 <rayrtsx@proton.me>
2024-04-28 21:00:59 -0600

    Update Astro adapter mode to standalone in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [auth(), tailwind(), icon()],
   output: "server",
   adapter: node({
-    mode: "middleware"
+    mode: "standalone"
   }),
   vite: {
     build: {
