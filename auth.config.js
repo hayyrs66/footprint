2ee48f66acb507215a173974f661c850c6d3dbbd
hayyrs66 <rayrtsx@proton.me>
2024-05-03 16:14:03 -0600

    access to user ID

--- auth.config.js
+++ auth.config.js
@@ -24,18 +24,16 @@ export default defineConfig({
 
   callbacks: {
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
       session.accessToken = token.accessToken
-      session.user.id = token.id
+      session.user.id = token.sub
       
       return session
     },
     
-    async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
+    async jwt({ token, account, profile, user }) {
       if (account) {
         token.accessToken = account.access_token
-        token.id = profile.id
+        token.sub = profile.id
       }
       return token
     }
