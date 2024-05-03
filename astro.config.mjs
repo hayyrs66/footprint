c017f662bf9039641541da1fc51e831df536368e
hayyrs66 <rayrtsx@proton.me>
2024-05-03 11:56:38 -0600

    Add @astrojs/db dependency and integrate it in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), tailwind(), icon()],
+  integrations: [auth(), tailwind(), icon(), db()],
   output: "server",
   adapter: node({
     mode: "standalone"
