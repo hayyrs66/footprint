ab1ffa9a193dbedd358bd8e073044dbf8291fe3c
hayyrs66 <rayrtsx@proton.me>
2024-05-05 22:23:48 -0600

    added one request per day to avoid funny people

--- src/pages/api/fetchData.ts
+++ src/pages/api/fetchData.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import type { APIContext } from "astro";
-import { db, Storage, NOW } from "astro:db";
+import { db, Storage, NOW, eq } from "astro:db";
 import { getSession } from "auth-astro/server";
 
 const res = (
@@ -9,10 +9,14 @@ const res = (
     status,
     statusText,
     headers,
-  }: { status: number; statusText?: string; headers?: Headers }
+  }: { status: number; statusText?: string; headers?: HeadersInit }
 ): Response => new Response(body, { status, statusText, headers });
 
-export const GET: APIRoute = async ({ params, request }: APIContext) => {
+export const GET: APIRoute = async ({
+  params,
+  request,
+  redirect,
+}: APIContext) => {
   const session = (await getSession(request)) as any;
 
   if (!session || session?.user == null) {
@@ -38,11 +42,66 @@ export const GET: APIRoute = async ({ params, request }: APIContext) => {
         storageQuota: { usage, limit },
       } = data;
 
-      const storage = { userId, storage: usage, storageLimit: limit, calculatedAt: NOW };
+      const storage = {
+        userId,
+        storage: usage,
+        storageLimit: limit,
+        calculatedAt: NOW,
+      };
 
-      await db.insert(Storage).values(storage);
+      try {
+        await db.insert(Storage).values(storage);
+      } catch (insertionError) {
+        console.error(
+          "Error al insertar datos en la base de datos:",
+          insertionError
+        );
+        return res("Error al insertar datos en la base de datos", {
+          status: 500,
+        });
+      }
 
-      return res("Data retrieved correctly", { status: 200 });
+      try {
+        // we verify that the user can only make one request per day
+
+        const today = new Date();
+
+        const lastStorageData = await db
+          .select()
+          .from(Storage)
+          .where(eq(Storage.userId, userId));
+
+        if (lastStorageData !== null && lastStorageData.length > 1) {
+          console.log("hasta aqui actuo", lastStorageData);
+
+          const lastStorageDate = new Date(
+            lastStorageData[lastStorageData.length - 1].calculatedAt
+          );
+
+          // we are gonna calculate the difference between the last time the user made a request and today
+          // and if the difference is less than 24 hours we are gonna return a 400 status code
+
+          const difference = today.getTime() - lastStorageDate.getTime();
+          const differenceInHours = difference / (1000 * 3600);
+
+          if (differenceInHours < 24) {
+            return res(
+              "You can only make one request per day. Please try again later.",
+              { status: 400 }
+            );
+          }
+        }
+      } catch (error) {
+        console.error(
+          "Error al verificar la cantidad de solicitudes por día:",
+          error
+        );
+        return res("Error al verificar la cantidad de solicitudes por día", {
+          status: 500,
+        });
+      }
+
+      return redirect("/dashboard", 302);
     } else {
       console.error("No se pudo obtener el token de acceso.");
     }
